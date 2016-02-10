2a6c91cf4afb8ea355b9217881b0a5b6f1cb0180
Jack Doan <me@jackdoan.com>
2016-02-10 16:14:01 -0500

    it's been awhile

---
+++ amazon/index.php
@@ -0,0 +1,5 @@
+<?php
+header("Location: https://www.amazon.com/gp/registry/ref=cm_wl_edit_brc?ie=UTF8&id=1QXELPC652CLG&type=wishlist");
+die();
+//https://www.amazon.com/gp/registry/ref=cm_wl_edit_brc?ie=UTF8&id=1QXELPC652CLG&type=wishlist
+?>
