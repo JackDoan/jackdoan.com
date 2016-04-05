254fc7a0697e12f6347b8909f39054a6b4a9b078
Jack Doan <me@jackdoan.com>
2016-04-04 21:29:25 -0400

    leaving old server

---
+++ 3390/index.php
@@ -0,0 +1,7 @@
+<?php
+header("Location: https://docs.google.com/presentation/d/1cR2ffgN6Z8uEhTaX0tU5k1Yi9qQPYs9IpqhkweDq9A0/edit?usp=sharing");
+die();
+//https://www.amazon.com/gp/registry/ref=cm_wl_edit_brc?ie=UTF8&id=1QXELPC652CLG&type=wishlist
+?>
+
+<a href="https://docs.google.com/presentation/d/1cR2ffgN6Z8uEhTaX0tU5k1Yi9qQPYs9IpqhkweDq9A0/edit?usp=sharing">here</a>
